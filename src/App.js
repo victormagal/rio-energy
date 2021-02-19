79447f47c2707d90d3d5e8162f5fab7fb8496e8f
Antonio Junior <juniordsj@gmail.com>
2021-02-19 11:07:41 -0300

    criação da estrutura do component footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 
+import Footer from './components/Footer';
+
 function App() {
   return (
     <div className="App">
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer">
+        <div img="" alt="Logo Rio Energy Footer"></div>
+        <h3>Vamos conversar?</h3>
+
+        <div className="footer__social">
+          <a href="http://">LinkedIn</a>
+          <a href="http://">Facebook</a>
+        </div>
+
+        <p className="footer__copyright">Rio Energy® Todos os direitos reservados</p>
+        <p className="footer__infor">Desenvolvido pela AC</p>
+
+        <div className="footer__links">
+          <a href="http://">Início</a>
+          <a href="http://">Quem somos</a>
+          <a href="http://">Nossa energia</a>
+          <a href="http://">Nosso impacto</a>
+          <a href="http://">Carreira</a>
+          <a href="http://">Investidores</a>
+          <a href="http://">Contato</a>
+        </div>
+
+      </footer>
+    </>
+  )
+}
+
+export default Footer;
