ec13a7dbb495430b915bf47fe5a8f2dbebbc8054
Fabricio Soares <eu@fabriciosoares.com>
2021-02-20 11:46:30 -0300

    adicionado fonte source sans

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,26 @@ module.exports = {
     extend: {},
     textColor: theme => theme('colors'),
     textColor: {
-      'primary': '#3490dc',
-      'secondary': '#ffed4a',
-      'danger': '#e3342f',
+      // 'primary': '#3490dc',
+      // 'secondary': '#ffed4a',
+      // 'danger': '#e3342f', degular source sans pro
       'green-light': '#C3F37B',
+      'blue-sky-night': '#191B87',
       'dark': '#12121D',
+      'gray': '#4A4A57',
+      'gray-light': '#ECF1F4',
+      'gray-mid': '#88889C',
+      'clear': '#FAFCFE',
       'white': '#FFFFFF',
+      'iris-10': '#EFEFFD',
+      'green-power': '#00D35C',
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+
+  ],
 }
