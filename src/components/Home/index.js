bc456b8d69503b03094cf723d2250078d414de15
Fabricio Soares <eu@fabriciosoares.com>
2021-02-24 13:48:43 -0300

    feat(cards): exemplo estrutura

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div>
       <div className="card-duplo xl:container mx-auto border-2 border-green-light grid grid-rows-1 md:grid-cols-2">
-        <div className="card border-2 border-blue-sky-night">
+        <div className="card">
           <h3>Nossa Energia</h3>
           <h2>Conheça nossas soluções e diferenciais para você</h2>
           <button>
             Saiba mais
           </button>
         </div>
-        <div className="card border-2 border-blue-sky-night">
+        <div className="card">
           <h3>Nossa Energia</h3>
           <h2>Conheça nossas soluções e diferenciais para você</h2>
           <button>
@@ -21,7 +21,62 @@ export default function Home() {
           </button>
         </div>
       </div>
-      <div className="subfooter w-auto h-40"></div>
+
+      <div className="subfooter w-auto xl:container font-sans mx-auto my-4 text-center">
+        <h3 className="text-4xl	text-blue-sky-night font-bold my-6">#energiaquenosmove</h3>
+        <div className="slider flex flex-row flex-nowrap justify-center gap-4 overflow-x-auto">
+        <div className="w-60 border border-green-light rounded-2xl px-6 py-6 mx-auto">
+          <h4 className="text-blue-sky-night">Há 1 dia</h4>
+          <p className="text-gray-mid">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt ex vel massa consequat, eget dapibus risus sollicitudin volutpat.</p>
+          <a className="text-blue-sky-night" href="">Linkedin</a>
+          <span className="text-blue-sky-night">@rioenergy</span>
+        </div>
+        <div className="w-60 border border-green-light rounded-2xl px-6 py-6 mx-auto">
+          <h4 className="text-blue-sky-night">Há 1 dia</h4>
+          <p className="text-gray-mid">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt ex vel massa consequat, eget dapibus risus sollicitudin volutpat.</p>
+          <a className="text-blue-sky-night" href="">Linkedin</a>
+          <span className="text-blue-sky-night">@rioenergy</span>
+        </div>
+        <div className="w-60 border border-green-light rounded-2xl px-6 py-6 mx-auto">
+          <h4 className="text-blue-sky-night">Há 1 dia</h4>
+          <p className="text-gray-mid">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt ex vel massa consequat, eget dapibus risus sollicitudin volutpat.</p>
+          <a className="text-blue-sky-night" href="">Linkedin</a>
+          <span className="text-blue-sky-night">@rioenergy</span>
+        </div>
+        <div className="w-60 border border-green-light rounded-2xl px-6 py-6 mx-auto">
+          <h4 className="text-blue-sky-night">Há 1 dia</h4>
+          <p className="text-gray-mid">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt ex vel massa consequat, eget dapibus risus sollicitudin volutpat.</p>
+          <a className="text-blue-sky-night" href="">Linkedin</a>
+          <span className="text-blue-sky-night">@rioenergy</span>
+        </div>
+        <div className="w-60 border border-green-light rounded-2xl px-6 py-6 mx-auto">
+          <h4 className="text-blue-sky-night">Há 1 dia</h4>
+          <p className="text-gray-mid">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt ex vel massa consequat, eget dapibus risus sollicitudin volutpat.</p>
+          <a className="text-blue-sky-night" href="">Linkedin</a>
+          <span className="text-blue-sky-night">@rioenergy</span>
+        </div>
+        <div className="w-60 border border-green-light rounded-2xl px-6 py-6 mx-auto">
+          <h4 className="text-blue-sky-night">Há 1 dia</h4>
+          <p className="text-gray-mid">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt ex vel massa consequat, eget dapibus risus sollicitudin volutpat.</p>
+          <a className="text-blue-sky-night" href="">Linkedin</a>
+          <span className="text-blue-sky-night">@rioenergy</span>
+        </div>
+        <div className="w-60 border border-green-light rounded-2xl px-6 py-6 mx-auto">
+          <h4 className="text-blue-sky-night">Há 1 dia</h4>
+          <p className="text-gray-mid">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt ex vel massa consequat, eget dapibus risus sollicitudin volutpat.</p>
+          <a className="text-blue-sky-night" href="">Linkedin</a>
+          <span className="text-blue-sky-night">@rioenergy</span>
+        </div>
+        <div className="w-60 border border-green-light rounded-2xl px-6 py-6 mx-auto">
+          <h4 className="text-blue-sky-night">Há 1 dia</h4>
+          <p className="text-gray-mid">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt ex vel massa consequat, eget dapibus risus sollicitudin volutpat.</p>
+          <a className="text-blue-sky-night" href="">Linkedin</a>
+          <span className="text-blue-sky-night">@rioenergy</span>
+        </div>
+
+        </div>
+      </div>
+
       <Footer />
     </div>
   )
