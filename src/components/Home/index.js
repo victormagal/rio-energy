2d7e1b6b1ae7ded65bb5caabfaac2c8deac0fe4b
Antonio Junior <juniordsj@gmail.com>
2021-02-24 10:32:25 -0300

    feat: add estrutura do component header

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
 import Footer from '../Shared/Footer'
+import Header from '../Shared/Header'
+import CardImage from '../Shared/CardImage'
 
 export default function Home() {
   return (
     <div>
+      <Header />
+      <CardImage />
       <div className="card-duplo xl:container mx-auto border-2 border-green-light grid grid-rows-1 md:grid-cols-2">
         <div className="card border-2 border-blue-sky-night">
           <h3>Nossa Energia</h3>
