e1ca0b4bc91c220740b3f5baa8a319906edbf597
Fabricio Soares <eu@fabriciosoares.com>
2021-02-25 20:07:09 -0300

    feat(home): separação de conteudo

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -30,7 +30,7 @@ export default function Home() {
 
       <div className="subfooter w-auto xl:container font-sans mx-auto my-4 text-center">
         <h3 className="text-4xl	text-blue-sky-night font-bold my-6 break-words">#energiaquenosmove</h3>
-        <div className="slider container flex flex-nowrap overflow-x-scroll scrolling-touch justify-items-stretch gap-4 px-6 pr-6">
+        <div className="slider xl:container flex flex-nowrap overflow-x-scroll scrolling-touch justify-items-stretch gap-4 px-6 pr-6 mx-auto">
         <div className="w- border border-green-light rounded-2xl px-6 py-6 mx-auto">
           <h4 className="text-blue-sky-night">Há 1 dia</h4>
           <p className="text-gray-mid">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt ex vel massa consequat, eget dapibus risus sollicitudin volutpat.</p>
