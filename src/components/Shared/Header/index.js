e2e614cfab096f37b9268d8f8d14dedb507d3a08
Fabricio Soares <eu@fabriciosoares.com>
2021-02-25 20:07:47 -0300

    feat(header): ordem divs

--- src/components/Shared/Header/index.js
+++ src/components/Shared/Header/index.js
@@ -4,14 +4,21 @@ export default function Header() {
   return (
     <div>
       <div className="header">
-        <img src="" alt="" className="logo"/>
-        <button className="menu">X</button>
+        <div className="logo">
+          <img src="logo_header.svg" alt="" className="inline-block fill-current text-green-light mt-7 logo w-auto h-auto"/>
+    x     <div>
+            <h3>Rio Energy.Por um futuro mais limpo</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+        </div>
       </div>
-      <div>
-        <h3>Rio Energy.Por um futuro mais limpo</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </div>
+
+      <div className="menu">
+      <button className="menu">X</button>
+
       <button className="fale-conosco">...</button>
+
+    </div>
     </div>
   )
 }
