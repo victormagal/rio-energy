d9c13d1a3b1b20f86e1af0b2b4bb0db9796a922c
Antonio Junior <juniordsj@gmail.com>
2021-03-02 23:08:13 -0300

    feat: add img e subheader

--- src/components/Shared/ClientsPartners/index.js
+++ src/components/Shared/ClientsPartners/index.js
@@ -5,7 +5,7 @@ export default function ClientsPartners({ imgs }) {
     <div className="flex h-60 bg-gray-light font-sans">
       <div className="w-full self-center">
         <h3 className="text-2xl	text-blue-sky-night text-center font-bold break-words pb-7">Quem joga limpo com a gente</h3>
-        <div className="slider xl:container flex flex-nowrap overflow-x-scroll scrolling-touch justify-items-stretch gap-4">
+        <div className="slider xl:container flex flex-nowrap justify-items-stretch gap-4">
           <div className="flex flex-row">
             {imgs.map((value, i) => (
               <div className="ml-20">
