9902986ed3550f47851d18694757172f9643caae
Fabricio Soares <eu@fabriciosoares.com>
2021-03-04 02:24:50 -0300

    feat(menu): style wip

--- src/components/Shared/Menu/index.js
+++ src/components/Shared/Menu/index.js
@@ -2,64 +2,66 @@ import React from 'react'
 
 export default function Menu() {
   return (
-    <div>
-      <>
-        <menu>
-            <div className="logo-container">
-              {/* <a href="">
-                logo
-              </a> */}
-            </div>
-            <div className="icon-close">
-              {/* <a href="">
-                icon
-              </a> */}
-            </div>
-            <nav>
-              <ul>
-                <li>
-                  <a href="">Quem somos</a>
-                </li>
-                <li>
-                  <a href="">Nossa energia</a>
-                </li>
-                <li>
-                  <a href="">Nosso impacto</a>
-                </li>
-                <li>
-                  <a href="">Carreira</a>
-                </li>
-                <li>
-                  <a href="">Investidores</a>
-                </li>
-                <li>
-                  <a href="">Contato</a>
-                </li>
-              </ul>
-              <ul className="info-end">
-                <li>
-                  <p>Rua Jardim Botânico 518, 5º andar</p>
-                </li>
-                <li>
-                  <p>Jardim Botânico</p>
-                </li>
-                <li>
-                  <p>Rio de Janeiro, RJ – Brasil</p>
-                </li>
-              </ul>
-              <ul className="info-tel-email">
-                <li>
-                  <p>+55 21 3733-2975</p>
-                  <p>contato@rioenergy.com.br</p>
-                </li>
-              </ul>
-              <ul className="info-sociais">
-                <li><a href="http://">LinkedIn</a></li>
-                <li><a href="http://">Facebook</a></li>
-              </ul>
-            </nav>
-        </menu>
-      </>
+    <div className=" h-screen w-screen bg-green-power px-32 py-32">
+      <div className="logo-container">
+        {/* <a href="">
+          logo
+        </a> */}
+      </div>
+      <div className="icon-close">
+        {/* <a href="">
+          icon
+        </a> */}
+      </div>
+      <nav className="grid grid-cols-1 md:grid-cols-2 justify-items-center">
+        <ul className=" text-5xl mb-20 text-blue-sky-night font-serif">
+          <li className="mb-10">
+            <a href="">Início</a>
+          </li>
+          <li className="mb-10">
+            <a href="">Quem somos</a>
+          </li>
+          <li className="mb-10">
+            <a href="">Nossa energia</a>
+          </li>
+          <li className="mb-10">
+            <a href="">Nosso impacto</a>
+          </li>
+          <li className="mb-10">
+            <a href="">Carreira</a>
+          </li>
+          <li className="mb-10">
+            <a href="">Investidores</a>
+          </li>
+          <li className="mb-10">
+            <a href="">Contato</a>
+          </li>
+        </ul>
+        <div className="grid grid-cols-1 md:grid-cols-2 self-end justify-self-end text-white">
+        <ul className="info-end">
+          <li>
+            <p>Rua Jardim Botânico 518, 5º andar</p>
+          </li>
+          <li>
+            <p>Jardim Botânico</p>
+          </li>
+          <li>
+            <p>Rio de Janeiro, RJ – Brasil</p>
+          </li>
+        </ul>
+        <ul className="info-tel-email">
+          <li>
+            <p>+55 21 3733-2975</p>
+            <p>contato@rioenergy.com.br</p>
+          </li>
+        </ul>
+      </div>
+      </nav>
+      <ul className="info-sociais text-white flex items-stretch">
+          <li className=""><a href="http://">LinkedIn</a></li>
+          <li className=""><a href="http://">Facebook</a></li>
+        </ul>
+
     </div>
   )
 }
