87753cb83a359772e2e3a14b3136b3c6be1a097a
Ramon Henrique Ornelas <ramon.ornela@gmail.com>
2021-03-06 08:22:26 -0300

    fix: svg

--- src/components/Shared/Logo/index.js
+++ src/components/Shared/Logo/index.js
@@ -2,16 +2,18 @@ import React from 'react'
 import SvgHeader from '../Svg/Header'
 import SvgFooter from '../Svg/Footer'
 
-export default function Logo ({ withText, color, className, width, height })  {
+export default function Logo ({ withText, color, className, classNameSvg, viewBox, width, height })  {
   const Component = withText ? SvgHeader : SvgFooter
 
   return (
     <>
       <div className={className}>
         <Component
+          className={classNameSvg}
           color={color}
           width={width}
           height={height}
+          viewBox={viewBox}
         />
       </div>
     </>
