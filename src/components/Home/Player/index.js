4377e552d21309aa78853c3783414998ac9e6ba8
Victor Magalhaes <victormagalhaes@MacBook-Pro-de-Victor.local>
2021-03-08 21:06:33 -0300

    secao de video da home alterado para imagem estatica com texto sobreposto

--- src/components/Home/Player/index.js
+++ src/components/Home/Player/index.js
@@ -6,9 +6,10 @@ export default function Player() {
   return (
     <>
       <div className="player xl:container mx-auto p-10">
-        <div className="video rounded-3xl block bg-cover" style={{ height: '622px', backgroundImage: `url('${video}')`}} >
-          <div className="">
-
+        <div className="flex flex-col justify-end video rounded-3xl block bg-cover" style={{ height: '622px', backgroundImage: `url('${video}')`}} >
+          <div className="flex flex-col pb-6">
+            <h1 className="text-center font-serif font-bold text-7xl text-white">Energia para um futuro sustentável</h1>
+            <h2 className="text-center font-sans font-semibold text-3xl text-white">Nossa energia cresce de mãos dadas com nosso impacto.</h2>
           </div>
         </div>
       </div>
