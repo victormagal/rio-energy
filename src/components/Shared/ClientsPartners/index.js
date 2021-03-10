42035022d4268765a2b29b8fabc7260529d624ff
Ramon Henrique Ornelas <ramon.ornela@gmail.com>
2021-03-09 22:40:25 -0300

    fix: scroll rodape

--- src/components/Shared/ClientsPartners/index.js
+++ src/components/Shared/ClientsPartners/index.js
@@ -3,18 +3,23 @@ import Scroll from '../Scroll'
 
 export default function ClientsPartners({ imgs, title = 'Same Energy, nossos clientes' }) {
   return (
-    <div className="flex h-60 bg-gray-light font-sans">
+    <div className="flex bg-gray-light font-sans">
       <div className="w-full self-center">
-        <h3 className="text-2xl	text-blue-sky-night text-center font-bold break-words pb-7">{title}</h3>
-        <div className="slider xl:container flex flex-nowrap justify-items-stretch gap-4">
-          <Scroll className="flex flex-row">
+        <h3 className="text-2xl	text-blue-sky-night text-center font-bold break-word pt-32">{title}</h3>
+
+        <Scroll className="py-24">
+          <div className="flex flex-row">
             {imgs.map((value, i) => (
-              <div className="ml-20">
-                <img src={value} alt={value} className="w-32" key={i} />
+              <div className="pr-10">
+                <div
+                  className="cover bg-no-repeat w-32 h-32"
+                  style={{ backgroundImage: `url(${value})` }}
+                />
               </div>
             ))}
-          </Scroll>
-        </div>
+          </div>
+        </Scroll>
+
       </div>
     </div>
   )
