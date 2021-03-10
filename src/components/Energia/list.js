a70ab292046be3dc4455c364478d2c129b159f35
Victor Magalhaes <victormagalhaes@MacBook-Pro-de-Victor.local>
2021-03-09 23:57:25 -0300

    card de energia inserido

--- src/components/Energia/list.js
+++ src/components/Energia/list.js
@@ -2,12 +2,12 @@ import Scroll from '../Shared/Scroll';
 
 function Card({ img, description }) {
   return (
-    <div style={{ height: '300px' }} className="w-52 bg-white text-blue-sky-night mr-5 rounded-3xl flex flex-col items-center">
+    <div style={{ height: '380px' }} className="w-80 bg-white text-blue-sky-night mr-5 rounded-3xl flex flex-col items-center">
       <div>
         <img src={img} alt="" width="150" />
       </div>
       <div className="px-8">
-        <p className="font-sans font-normal text-dark text-base pt-2">{description}</p>
+        <p className="font-serif font-bold text-2xl text-dark pt-2">{description}</p>
       </div>
     </div>
   )
