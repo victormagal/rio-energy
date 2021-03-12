c7646024d63c82fb290cd7b22665ba0201b4916b
Victor Magalhaes <victormagalhaes@MacBook-Pro-de-Victor.local>
2021-03-12 01:10:44 -0300

    erro de lint sobre texto alternativo na tag de imagem corrigido

--- src/components/Shared/CardList/card.js
+++ src/components/Shared/CardList/card.js
@@ -4,7 +4,7 @@ export default function Card({ img, title, description }) {
   return (
     <div className="bg-iris-10 flex px-9 mb-9 rounded-2xl">
       <div className="w-4/12">
-        <img className='w-52' src={img} />
+        <img className='w-52' src={img} alt="" />
       </div>
       <div className="w-8/12 pt-4">
         <h3 className="font-serif font-bold text-2xl text-blue-sky-night">{title}</h3>
