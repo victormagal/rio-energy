76bc0b4778dc7f6235c22305313dcb055ba845e1
Ramon Henrique Ornelas <ramon.ornela@gmail.com>
2021-03-12 00:54:26 -0300

    feat: fix logo z-indez

--- src/components/Shared/Header/index.js
+++ src/components/Shared/Header/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import SvgLogo from '../Logo'
 import Menu from './Menu'
 import MenuComponent from '../Menu'
@@ -9,7 +9,13 @@ export default function Header({ image, className, children, colorLogo }) {
 
   const [openMenu, setOpenMenu] = useState(false)
 
+  const [bgLogo, setBgLogo] = useState(colorLogo)
+
   const handleToggleMenu = () => setOpenMenu((previous) => !previous)
+
+  useEffect(() => {
+    openMenu ? setBgLogo('white') : setBgLogo(colorLogo)
+  }, [openMenu])
 
   if (image) {
     style.backgroundImage = `url('${image}')`
@@ -22,11 +28,11 @@ export default function Header({ image, className, children, colorLogo }) {
       {openMenu && <MenuComponent />}
       <div style={{ height: '720px', ...style }} className={className || ''}>
         <div className={`cover header grid grid-cols-12 pt-8 xl:container mx-auto gap-4`}>
-          <div className="col-span-11">
+          <div className="col-span-11 relative z-50">
             <Link to="/">
               <SvgLogo
                 withText={true}
-                color={colorLogo}
+                color={bgLogo}
                 width={177}
                 height={68}
                 viewBox="-10 200 520 100"
